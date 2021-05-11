1e11ffa133278d1a65f0ef119c08b35ece79c5ef
fatih <farslan0699@gmail.com>
2021-05-11 12:09:33 +0300

    code git push git push origin main

--- lib/main.dart
+++ lib/main.dart
@@ -23,17 +23,104 @@ class LoginPage extends StatelessWidget {
         body: Center(
           child: Column(
             children: <Widget>[
-              SizedBox(height: 50.0,),
+              SizedBox(
+                height: 50.0,
+              ),
               FlutterLogo(
                 size: 70.0,
                 textColor: Colors.grey,
               ),
-              SizedBox(height: 25.0,),
+              SizedBox(
+                height: 25.0,
+              ),
               Text("Social World",
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 30.0,
-                      fontWeight: FontWeight.bold))
+                      fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 35.0,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Mail İle Giriş",
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        width: double.infinity,
+                        height: 52.0,
+                        decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "Facebook İle Giriş",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              height: 52.0,
+                              decoration: BoxDecoration(
+                                  color: Colors.indigo,
+                                  borderRadius: BorderRadius.circular(20.0)),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "Gmail İle Giriş",
+                                style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              height: 52.0,
+                              decoration: BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius: BorderRadius.circular(20.0)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.purple[300],
+                          Colors.purple[100],
+                        ]),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50.0)),
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width / 0.8,
+                height: 180.0,
+              )
             ],
           ),
         ));
